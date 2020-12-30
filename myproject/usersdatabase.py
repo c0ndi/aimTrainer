85931562f018a2572c515ebf067e47593897c1f3
Piotrek <piotrgszkola@gmail.com>
2020-12-30 20:30:24 +0100

    Update usersdatabase.py

--- myproject/usersdatabase.py
+++ myproject/usersdatabase.py
@@ -9,7 +9,7 @@ class Users(db.Model):
     nick = db.Column(db.Text)
     password = db.Column(db.Text)
     max_score = db.Column(db.Integer)
-    
+
 
     def __init__(self, login, nick, password):
         self.login = login
@@ -23,13 +23,6 @@ class Users(db.Model):
         users = (Users.query.filter_by(login = self.login, nick = self.nick)).all()
 
         if not self.login and not self.nick:
-            return True
-        else:
-            return False
-
-    def valid(login, password):
-        users = (Users.query.filter_by(login = login, password = password)).all()
-        if not login and not password:
             return True
         else:
             return False
